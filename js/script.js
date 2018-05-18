ad86b4154f315560018330bbc06fd701869d32f0
raekwonza <34821012+raekwonza@users.noreply.github.com>
2018-05-18 10:54:56 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+menu.onclick = function myFunction() {
+	let x = document.getElementById('myTopNav');
+
+	if (x.className === 'topnav') {
+		x.className += ' responsive';
+	}
+	else {
+		x.className = 'topnav';
+	}
+}
